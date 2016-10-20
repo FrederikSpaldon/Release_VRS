25cf74c30db023c08564d77e5d75eaa90c9f6381
FrederikSpaldon <spaldon.frederik@gmail.com>
2016-10-20 16:34:21 +0200

    CV5_uloha1 prerusenia

--- vrs_cv4/src/main.c
+++ vrs_cv4/src/main.c
@@ -47,6 +47,8 @@ SOFTWARE.
 **===========================================================================
 */
 
+uint16_t AD_value;
+
 //init klavesnice
 void adc_init(void)
 {
@@ -69,10 +71,14 @@ void adc_init(void)
  ADC_StructInit(&ADC_InitStructure);
  /* ADC1 configuration */
  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
- ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+ ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
  ADC_InitStructure.ADC_NbrOfConversion = 1;
+
+ ADC_ITConfig(ADC1, ADC_IT_EOC , ENABLE);
+ //ADC_ITConfig(ADC1,  ADC_IT_OVR, ENABLE);
+
  ADC_Init(ADC1, &ADC_InitStructure);
 /* ADCx regular channel8 configuration */
  ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_16Cycles);
@@ -96,7 +102,34 @@ void init_LED(void){
 	gpioInitStruc.GPIO_Speed=GPIO_Speed_400KHz;
 	GPIO_Init(GPIOA,&gpioInitStruc);
 
+
 }
+
+void init_NVIC(void){
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = ADC1_IRQn; //zoznam prerušení nájdete v súbore stm32l1xx.h
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 4;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&NVIC_InitStructure);
+
+	//ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC || ADC_FLAG_OVR);
+}
+
+void ADC1_IRQHandler(void){
+	//while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC | ADC_FLAG_OVR)!=RESET){}
+
+		//GPIO_SetBits(GPIOA, GPIO_Pin_5);
+
+		if(ADC1->SR && ADC_SR_EOC){
+			AD_value = ADC1->DR;
+		}
+
+
+}
+
 
 //delay funkcia
 void blikaj(int pocet){
@@ -141,16 +174,18 @@ void casovac(uint16_t AD_value){
 int main(void)
 {
 
+  AD_value=0;
+
   adc_init();
   init_LED();
+  init_NVIC();
   // Infinite loop */
-  uint16_t AD_value=(uint16_t)0;
+
 
   while(1){
-	  ADC_SoftwareStartConv(ADC1);
-	  while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)){}
-	  AD_value=ADC_GetConversionValue(ADC1);
-	  casovac(AD_value);
+	  //
+	  //while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC)){}
+	 casovac(AD_value);
   }
   return 0;
 }
